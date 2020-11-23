7d5ced58f9bc4765c739aeed9e682acba943b459
Tomek Marcinkowski <tomasz.marcinkowski@rst.com.pl>
2020-11-23 13:47:41 +0100

    added: simple write to a table

--- functions/financial-risk.ts
+++ functions/financial-risk.ts
@@ -2,23 +2,37 @@
 
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import fetch from 'node-fetch';
+import * as DynamoDB from 'aws-sdk/clients/dynamodb';
+import assert = require('assert');
+
+assert.ok(process.env.SOURCE_URI, 'No SOURCE_URI env variable!');
+assert.ok(process.env.COMPANIES_TABLE, 'Table name is not provided.');
 
 const getUriForCompany = (companyId: number) => {
-    if (!process.env.SOURCE_URI) {
-        throw new Error('No SOURCE_URI env variable!');
-    }
-
-    return process.env.SOURCE_URI.replace('{id}', `${companyId}`);
+    return process.env.SOURCE_URI!.replace('{id}', `${companyId}`);
 };
 
 const handler: APIGatewayProxyHandler = async event => {
     const id = parseInt(event.pathParameters?.id || '', 10);
     const response = await fetch(getUriForCompany(id));
+    const dbClient = new DynamoDB.DocumentClient();
+    const lastFetchedAt = (new Date()).toISOString();
 
     if (response.ok) {
+        const companyInfo = await response.json();
+
+        await dbClient.put({
+            TableName: process.env.COMPANIES_TABLE!,
+            Item: {
+                companyId: id,
+                lastFetchedAt,
+                ...companyInfo,
+            }
+        }).promise();
+
         return {
             statusCode: response.status,
-            body: JSON.stringify(await response.json(), null, 2),
+            body: JSON.stringify(companyInfo, null, 2),
         };
     } else {
         return {
